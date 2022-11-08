a145ff4e398d38f95e85d9a479b2a1e290cdbb56
z28256 <18836930989@163.com>
2022-11-08 08:51:01 +0800

    Update array.c
    增加数组课程关于数组名的内容

--- first_class/first_class/array.c
+++ first_class/first_class/array.c
@@ -33,74 +33,93 @@
 //}
 
 
-#include "game.h"
-void menu()
-{
-	printf("0.exit\n");
-	printf("1.play\n");
-}
-void game()
-{
-	char ret = 0;
-	char board[ROW][COL] = { 0 };
-	init_board(board, ROW, COL);
-	print_board(board, ROW, COL);
-	while (1)
-	{
-		player_move(board, ROW, COL);
-		print_board(board, ROW, COL);
-		ret = is_win(board, ROW, COL);
-		if (ret != 'C')
-		{
-			break;
-		}
-		computer_move(board, ROW, COL);
-		print_board(board, ROW, COL);
-		ret = is_win(board, ROW, COL);
-		if (ret != 'C')
-		{
-			break;
-		}
+//数组名是首元素的地址
+//两个例外：
+//sizeof(数组名)，这里的数组名表示整个数组，计算的是整个数组大小
+//&数组名，这里的数组名也表示整个数组，取出的是整个数组的地址
+//地址大小：4字节（X86）  8字节（X64）
+//#include <stdio.h>
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	printf("%p\n", arr);
+//	printf("%p\n", arr + 1);
+//	printf("%p\n", &arr[0]);
+//	printf("%p\n", &arr[0] + 1);
+//	printf("%p\n", &arr);
+//	printf("%p\n", &arr + 1);
+//	return 0;
+//}
 
-	}
-	if (ret == '*')
-	{
-		printf("玩家赢了\n");
-	}
-	else if (ret == '#')
-	{
-		printf("电脑赢了\n");
-	}
-	else if (ret == 'Q')
-	{
-		printf("平局\n");
-	}
-}
 
-void test()
-{
-	int input = 0;
-	srand((unsigned int)time(NULL));
-	do
-	{
-		menu();
-		scanf("%d", &input);
-		switch (input)
-		{
-		case 1:
-			game();
-			break;
-		case 0:
-			printf("退出游戏\n");
-			break;
-		default:
-			printf("重新输入\n");
-			break;
-		}
-	} while (input);
-}
-int main()
-{
-	test();
-	return 0;
-}
+//#include "game.h"
+//void menu()
+//{
+//	printf("0.exit\n");
+//	printf("1.play\n");
+//}
+//void game()
+//{
+//	char ret = 0;
+//	char board[ROW][COL] = { 0 };
+//	init_board(board, ROW, COL);
+//	print_board(board, ROW, COL);
+//	while (1)
+//	{
+//		player_move(board, ROW, COL);
+//		print_board(board, ROW, COL);
+//		ret = is_win(board, ROW, COL);
+//		if (ret != 'C')
+//		{
+//			break;
+//		}
+//		computer_move(board, ROW, COL);
+//		print_board(board, ROW, COL);
+//		ret = is_win(board, ROW, COL);
+//		if (ret != 'C')
+//		{
+//			break;
+//		}
+//
+//	}
+//	if (ret == '*')
+//	{
+//		printf("玩家赢了\n");
+//	}
+//	else if (ret == '#')
+//	{
+//		printf("电脑赢了\n");
+//	}
+//	else if (ret == 'Q')
+//	{
+//		printf("平局\n");
+//	}
+//}
+//
+//void test()
+//{
+//	int input = 0;
+//	srand((unsigned int)time(NULL));
+//	do
+//	{
+//		menu();
+//		scanf("%d", &input);
+//		switch (input)
+//		{
+//		case 1:
+//			game();
+//			break;
+//		case 0:
+//			printf("退出游戏\n");
+//			break;
+//		default:
+//			printf("重新输入\n");
+//			break;
+//		}
+//	} while (input);
+//}
+//int main()
+//{
+//	test();
+//	return 0;
+//}
